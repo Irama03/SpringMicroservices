64ab4b748a0d12770fba355b5b99ede13974182c
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2022-10-31 00:48:30 +0200

    Created K8s resource files, replaced postgresql db by h2 in-memory db, other minor updates

--- chat-service/src/main/java/com/example/chatservice/configurations/WebClientConfig.java
+++ chat-service/src/main/java/com/example/chatservice/configurations/WebClientConfig.java
@@ -1,17 +1,22 @@
 package com.example.chatservice.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-    private static final String businessLogicServiceURL = "http://localhost:8080/api/";
+    @Autowired
+    private Environment environment;
 
     @Bean
     public WebClient getWebClient() {
-        return WebClient.builder().baseUrl(businessLogicServiceURL).build();
+        return WebClient.builder().baseUrl("http://" + environment.getProperty("container.name") +
+                ":" + environment.getProperty("container.port") + "/api/").build();
     }
 
 }
