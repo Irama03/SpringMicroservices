3e6447c237fbffa6af50e7971927b91b86533a36
illia_sitkov <illiasitkov@gmail.com>
2022-11-05 23:36:08 +0200

    feat: for business-logic app add jwt token validation filter, set up access rules for endpoints

--- businessLogic/src/main/java/com/example/businessLogic/controllers/LessorController.java
+++ businessLogic/src/main/java/com/example/businessLogic/controllers/LessorController.java
@@ -2,8 +2,9 @@ package com.example.businessLogic.controllers;
 
 import com.example.businessLogic.dtos.lessor.LessorPostDto;
 import com.example.businessLogic.models.Lessor;
-import org.springframework.validation.annotation.Validated;
 import com.example.businessLogic.services.interfaces.LessorService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,32 +21,38 @@ public class LessorController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN','CLIENT','LESSOR')")
     public Iterable<Lessor> getAll() {
         return lessorService.getAll();
     }
 
     @GetMapping(path = "/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','CLIENT','LESSOR')")
     public Lessor getById(@PathVariable long id) {
         return lessorService.getById(id);
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN','LESSOR')")
     public Lessor create(@Valid @RequestBody LessorPostDto lessor) {
         System.out.println(lessor);
         return lessorService.create(lessor);
     }
 
     @PutMapping(path = "/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','LESSOR')")
     public Lessor update(@PathVariable long id, @Valid @RequestBody LessorPostDto lessor) {
         return lessorService.updateById(id, lessor);
     }
 
     @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','LESSOR')")
     public void deleteById(@PathVariable long id) {
         lessorService.deleteById(id);
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public void deleteAll() {
         lessorService.deleteAll();
     }
