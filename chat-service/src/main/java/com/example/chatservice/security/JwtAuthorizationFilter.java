a6f285277274b22077676f86200717ef6db9ccc0
illia_sitkov <illiasitkov@gmail.com>
2022-11-05 23:46:25 +0200

    feat: for chat-service add jwt token validation filter, set up access rules for endpoints

---
+++ chat-service/src/main/java/com/example/chatservice/security/JwtAuthorizationFilter.java
@@ -0,0 +1,56 @@
+package com.example.chatservice.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.chatservice.helpers.ResponseHelper;
+import com.example.chatservice.models.ApiErrorResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+
+    @Autowired
+    private Environment environment;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (token != null) {
+            try {
+                String secret = "secret";// todo: environment is null for some reason
+                Algorithm algorithm = Algorithm.HMAC256(secret.getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(token);
+                String username = decodedJWT.getSubject();
+                Collection<GrantedAuthority> roles = List.of(new SimpleGrantedAuthority(decodedJWT.getClaim("role").asString()));
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, roles);
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                filterChain.doFilter(request, response);
+            } catch (Exception exception) {
+                ResponseHelper.setResponse(response, FORBIDDEN.value(), new ApiErrorResponse("Token validation failed"));
+            }
+        } else {
+            ResponseHelper.setResponse(response, FORBIDDEN.value(), new ApiErrorResponse("Missing token"));
+        }
+    }
+}
