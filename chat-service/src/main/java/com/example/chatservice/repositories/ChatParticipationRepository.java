9dfdf96de2f8f92edc9c1e850134054f05455027
vdzhos <dgosan37@gmail.com>
2022-11-13 23:01:46 +0200

    Changed logic for adding chats to be upsert-like (if chat for the given users exists - return it, else - create a new one).

--- chat-service/src/main/java/com/example/chatservice/repositories/ChatParticipationRepository.java
+++ chat-service/src/main/java/com/example/chatservice/repositories/ChatParticipationRepository.java
@@ -2,8 +2,18 @@ package com.example.chatservice.repositories;
 
 import com.example.chatservice.models.ChatParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface ChatParticipationRepository extends JpaRepository<ChatParticipation, Long> {
+
+    @Query(value =
+            "SELECT chat_id\n" +
+            "FROM chat_participation\n" +
+            "WHERE user_id = :userId", nativeQuery = true)
+    Set<Long> findChatParticipationsByUserId(Long userId);
 }
--- chat-service/src/main/java/com/example/chatservice/services/interfaces/ChatService.java
+++ chat-service/src/main/java/com/example/chatservice/services/interfaces/ChatService.java
@@ -3,14 +3,19 @@ package com.example.chatservice.services.interfaces;
 import com.example.chatservice.dtos.chats.ChatPostDto;
 import com.example.chatservice.models.Chat;
 
+import java.util.Optional;
+import java.util.Set;
+
 public interface ChatService {
 
-    Chat add(ChatPostDto chatPostDto);
+    Chat add(ChatPostDto chatPostDto, String authToken);
 
     Chat getById(Long id);
 
     void deleteById(Long id);
 
     Iterable<Chat> getAll();
+
+    Optional<Chat> getByUserIds(Set<Long> userIds);
 
 }
