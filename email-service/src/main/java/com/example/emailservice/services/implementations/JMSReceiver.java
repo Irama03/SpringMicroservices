4d9296e8c4825f992af87de7c9d352d7b10aca87
illia_sitkov <illiasitkov@gmail.com>
2022-11-18 16:17:06 +0200

    feat: add a service with Spring Admin, connect email-service to the admin-server, add Spring Actuator to email-service and businessLogic, disable JMS on email-service

--- email-service/src/main/java/com/example/emailservice/services/implementations/JMSReceiver.java
+++ email-service/src/main/java/com/example/emailservice/services/implementations/JMSReceiver.java
@@ -5,10 +5,12 @@ import com.example.emailservice.models.LogMessage;
 import com.example.emailservice.services.interfaces.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty(prefix = "jms", name = "enabled", havingValue = "true")
 public class JMSReceiver {
 
     @Autowired
@@ -20,7 +22,7 @@ public class JMSReceiver {
     @Value("${spring.mail.username}")
     private String to;
 
-    @JmsListener(destination = "LogsTopic", selector = "logLevel = 'INFO'")
+    @JmsListener(destination = "LogsTopic", selector = "logLevel = 'INFO' OR logLevel = 'WARN'")
     public void receiveMessage(LogMessage message) {
         emailService.sendEmail(new EmailMessage(from, to, message.getText(), "App email notification: "+message.getLevel()));
     }
