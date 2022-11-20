0661c680612fc6aded74604372693caadebb3807
soloway24 <illiakuznets@gmail.com>
2022-11-21 01:40:57 +0200

    Added a metric for average successful db call time (over the whole microservice)

--- chat-service/src/main/java/com/example/chatservice/metrics/RequestTimeTracker.java
+++ chat-service/src/main/java/com/example/chatservice/metrics/RequestTimeTracker.java
@@ -14,13 +14,20 @@ public class RequestTimeTracker {
     private double webClientOverallTime;
     private int webClientProceeds;
 
+    private double dbCallAllTime;
+    private int dbSuccessfulCalls;
+
+
     public void resetMetrics() {
         webClientOverallTime = 0;
         webClientProceeds = 0;
+
+        dbCallAllTime = 0;
+        dbSuccessfulCalls = 0;
     }
 
     private double trackTime(long millsStart) {
-        return (System.currentTimeMillis() - millsStart)/1000.;
+        return (System.currentTimeMillis() - millsStart) / 1000.;
     }
 
     @Around("execution(public * com.example.chatservice.apiCommunication.BusinessLogicWebClient.fetchUser(..))")
@@ -33,16 +40,32 @@ public class RequestTimeTracker {
         return res;
     }
 
+    @Around("execution(public * org.springframework.data.jpa.repository.JpaRepository+.*(..))")
+    public Object trackDBCallTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long current = System.currentTimeMillis();
+        final Object res = joinPoint.proceed();
+        double requestTime = trackTime(current);
+        dbCallAllTime += requestTime;
+        dbSuccessfulCalls++;
+        return res;
+    }
+
     private double getMetric(double time, int count) {
-        return count == 0 ? -1 : time/count;
+        return count == 0 ? -1 : time / count;
     }
 
     public double getAverageWebClientRequestTime() {
         return getMetric(webClientOverallTime, webClientProceeds);
     }
 
+    public double getAverageDbCallTime() {
+        return getMetric(dbCallAllTime, dbSuccessfulCalls);
+    }
+
     public Map<String, Double> getMetrics() {
-        return Map.of("avgWebClientSuccessfulRequestTime", getAverageWebClientRequestTime());
+        return Map.of(
+                "avgWebClientSuccessfulRequestTime", getAverageWebClientRequestTime(),
+                "avgDbSuccessfulCallTime", getAverageDbCallTime());
     }
 
 }
