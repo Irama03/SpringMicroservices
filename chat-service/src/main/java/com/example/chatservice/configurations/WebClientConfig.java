74a76caa73f03f481ce75220e9d31c6211b29426
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2022-12-04 19:53:39 +0200

    Added Eureka Server microservice; Made businessLogic and chat-service microservices eureka clients; Added loadbalancer to webclient in chat-service;

--- chat-service/src/main/java/com/example/chatservice/configurations/WebClientConfig.java
+++ chat-service/src/main/java/com/example/chatservice/configurations/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.example.chatservice.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -10,13 +11,22 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    private static final String BUSINESS_LOGIC_ID = "businessLogic";
+
     @Autowired
     private Environment environment;
 
     @Bean
-    public WebClient getWebClient() {
-        return WebClient.builder().baseUrl("http://" + environment.getProperty("container.name") +
-                ":" + environment.getProperty("container.port") + "/api/").build();
+    @LoadBalanced
+    public WebClient.Builder getWebClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient getWebClient(WebClient.Builder builder) {
+        return builder.baseUrl("http://" + BUSINESS_LOGIC_ID + "/api/").build();
+//        return WebClient.builder().baseUrl("http://" + environment.getProperty("container.name") +
+//                ":" + environment.getProperty("container.port") + "/api/").build();
     }
 
 }
